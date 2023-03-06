5289b23c125c48a4500818d626e0d8c81efea39e
Jahongir611 <khurramovjahongir611@gmail.com>
2023-03-06 18:36:57 +0500

    #

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 let doc = document
-// let h = doc.querySelector('h1')
-
-// black = () => {
-//     h.style.color = 'black'
-// }
-
-// h.addEventListener("mouseleave", black)
-// h.addEventListener("mousemove", function () { h.style.color = 'yellow' })
-
-// let btn = doc.querySelector('button')
-// btn.addEventListener('keyup', (event)=>{
-//     if(event.keyCode === 13){
-//         alert('Enter')
-//     }else if(event.keyCode === 32){
-//         alert('Пробел')
-//     }
-// })
 
 let next = doc.querySelector('.swiper-button-next')
 let prev = doc.querySelector('.swiper-button-prev')
@@ -44,14 +27,3 @@ new Swiper('.swiper', {
         enabled: true,
     },
 });
-
-// next.addEventListener('keyup', (event)=>{
-//     if(event.keyCode === 39){
-//             nextEl: '.swiper-button-next'
-//     }
-// })
-// prev.addEventListener('keyup', (event)=>{
-//     if(event.keyCode === 37){
-//             prevEl: '.swiper-button-prev'
-//     }
-// })
